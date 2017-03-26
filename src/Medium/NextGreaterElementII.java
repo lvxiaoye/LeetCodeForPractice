86a7850f8cc96a7dfbab36ddb36a3c9fde2af618
lvxiaoye <happylvye@gmail.com>
2017-03-27 00:08:15 +0200

    a

--- src/Medium/NextGreaterElementII.java
+++ src/Medium/NextGreaterElementII.java
@@ -15,7 +15,7 @@ public class NextGreaterElementII {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums={5,4,3,2,1};
+		int[] nums={2,-1,3,-1};
 		int[] res = nextGreaterElements(nums);
 		for(int i:res){
 			System.out.print(i+"　");
@@ -36,17 +36,15 @@ public class NextGreaterElementII {
     		if(nums[i]==max){
     			res[i]=-1;
     		}else{
-    			for(int j=i+1;j<n-1;j++){
+    			for(int j=i+1;j<n;j++){
     				if(nums[j]>nums[i]){
     					res[i]=nums[j];
-    					if(nums[n-1]==nums[i]){
-    						res[n-1]=res[i];
-    					}
     					break;
     				}else{
     					for(int k=0;k<i;k++){
     						if(nums[k]>nums[i]){
     							res[i]=nums[k];
+    							break;
     						}
     					}
     				}
@@ -54,8 +52,64 @@ public class NextGreaterElementII {
     		}
     		
     	}
+    	if(res[n-1]==0){
+    		if(nums[n-1]==max){
+    			res[n-1]=-1;
+    		}else{
+    			for(int k=0;k<n-1;k++){
+    				if(nums[k]>nums[n-1]){
+    					res[n-1]=nums[k];
+    					break;
+    				}
+    			}
+    		}
+    	}
     	
     	return res;
     }
+    /*
+     *   public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length, next[] = new int[n];
+        Arrays.fill(next, -1);
+        Stack<Integer> stack = new Stack<>(); // index stack
+        for (int i = 0; i < n * 2; i++) {
+            int num = nums[i % n]; 
+            while (!stack.isEmpty() && nums[stack.peek()] < num)
+                next[stack.pop()] = num;
+            if (i < n) stack.push(i);
+        }   
+        return next;
+    }
+     */
+    /*
+     *  public int[] nextGreaterElements(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        
+        int n = nums.length;
+        int[] result = new int[n];
+        int[] temp = new int[n * 2];
+        
+        for (int i = 0; i < n * 2; i++) {
+            temp[i] = nums[i % n];
+        }
+        
+        for (int i = 0; i < n; i++) {
+            result[i] = -1;
+            if (nums[i] == max) continue;
+            
+            for (int j = i + 1; j < n * 2; j++) {
+                if (temp[j] > nums[i]) {
+                    result[i] = temp[j];
+                    break;
+                }
+            }
+        }
+        
+        return result;
+    }
+     */
 
 }
